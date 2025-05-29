1701a1d17742f75d69140f8f3ac470436e691e51
Kabir S. Tamari <kabirtamari@gmail.com>
2025-05-29 11:17:09 +0000

    expnsee bug fixed

--- src/statements/apis/expense.py
+++ src/statements/apis/expense.py
@@ -29,9 +29,20 @@ class ExpenseItemViewSet(DefaultViewSet):
     lookup_field = 'id'
     search_fields = ['item_name']
 
-
     def get_queryset(self):
         expense_id = self.kwargs.get('expense_id')
         if expense_id:
             return ExpenseItem.objects.filter(expense_id=expense_id)
+        # If accessed directly without an expense_id, return none or handle as an error.
+        # For creation, we expect expense_id to be present.
         return ExpenseItem.objects.none()
+
+    def perform_create(self, serializer):
+        expense_id = self.kwargs.get('expense_id')
+        if not expense_id:
+            raise ValidationError("Expense ID must be provided in the URL.")
+        try:
+            expense_instance = Expense.objects.get(id=expense_id)
+        except Expense.DoesNotExist:
+            raise ValidationError(f"Expense with ID {expense_id} does not exist.")
+        serializer.save(expense=expense_instance)
